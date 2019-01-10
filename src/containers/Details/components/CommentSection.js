2ecdad21b027e1500000d4c765026c17dc8ce313
rubensv <Rubvalen@gmail.com>
2019-01-10 16:01:13 +0100

    Created CommentSection component.js

---
+++ src/containers/Details/components/CommentSection.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import { useState } from 'react';
+
+
+// Material UI components
+
+// Icons
+
+// Project components
+
+const styles = makeStyles({
+    root: {
+        padding: '22px',
+        backgroundColor: 'red',
+    }
+});
+
+const CommentSection = (props) => {
+    // State
+    const [data, setData] = useState({});
+
+    // Styling
+    const classes = styles();
+
+    return (
+        <div className={classes.root}>
+            <input value={data.email} onChange={(e) => setData({email: e.target.value})} />
+            <p>{data.email}</p>
+        </div>
+    )
+}
+
+export default (CommentSection);
