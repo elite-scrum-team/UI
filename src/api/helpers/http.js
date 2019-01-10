bd29f3a8cd20daa83c9a668969825d29af4ae72b
Shanshan Qu <530467167@qq.com>
2019-01-10 17:46:39 +0100

    make changes to login page and fix problems

--- src/api/helpers/http.js
+++ src/api/helpers/http.js
@@ -19,46 +19,42 @@ export default class Fetch {
     if (withAuth) {
       this.headers[TOKEN_HEADER_NAME] = "Bearer " + TOKEN.get();
     }
-
-    response(asForm=false) {
-        if(asForm) {
-            // Sends request with data as a FormData-object
-            return new Package(reqs.formRequest(this.method, this.url, this.headers, this.data));
-        } else {
-            // Sends a normal request
-            return new Package(reqs.request(this.method, this.url, this.headers, this.data));
-        }
-    }
   }
 
   response(asForm = false) {
     if (asForm) {
       // Sends request with data as a FormData-object
-      return reqs.formRequest(this.method, this.url, this.headers, this.data);
+      return new Package(
+        reqs.formRequest(this.method, this.url, this.headers, this.data)
+      );
     } else {
       // Sends a normal request
-      return reqs.request(this.method, this.url, this.headers, this.data);
+      return new Package(
+        reqs.request(this.method, this.url, this.headers, this.data)
+      );
     }
   }
 }
 
 class Package {
-    constructor(response) {
-        this.response = response.then((data) => {
-            if (!data) {
-                data = {};
-            }
+  constructor(response) {
+    this.response = response
+      .then(data => {
+        if (!data) {
+          data = {};
+        }
 
-            this.isError = !data.ok;
-            this.status = data.status;
-            
-            return (data.json)? data.json() : data;
-        }).catch((error) => console.log(error));
-    }
+        this.isError = !data.ok;
+        this.status = data.status;
 
-    then(method) {
-        return this.response.then(method);
-    }
+        return data.json ? data.json() : data;
+      })
+      .catch(error => console.log(error));
+  }
+
+  then(method) {
+    return this.response.then(method);
+  }
 }
 
 class Token {
--- src/api/services/AuthService.js
+++ src/api/services/AuthService.js
@@ -1,30 +1,30 @@
-import AUTH from '../auth';
-import { TOKEN } from '../helpers/http';
+import AUTH from "../auth";
+import { TOKEN } from "../helpers/http";
 
 // The userservice will do user related things,
 // and all the methods will return a promise
-export class AuthService {
-    static createUser = (email, password, callback) => {
-        // Trim and uncapitalize email
-        email = email.trim().toLowerCase();
+export default class AuthService {
+  static createUser = (email, password, callback) => {
+    // Trim and uncapitalize email
+    email = email.trim().toLowerCase();
 
-        const response = AUTH.createUser(email, password).response();
-        return response.then((data) => {
-            !callback || callback(response.isError, data);
-        });
-    };
+    const response = AUTH.createUser(email, password).response();
+    return response.then(data => {
+      !callback || callback(response.isError, data);
+    });
+  };
 
-    // The log in method,
-    static token = (email, password, callback) => {
-        // Trim and uncapitalize email
-        email = email.trim().toLowerCase();
+  // The log in method,
+  static token = (email, password, callback) => {
+    // Trim and uncapitalize email
+    email = email.trim().toLowerCase();
 
-        const response = AUTH.token(email, password).response();
-        return response.then((data) => {
-            if(response.isError === false && data && data.token) {
-                TOKEN.set(data.token);
-            }
-            !callback || callback(response.isError, data);
-        });
-    }
+    const response = AUTH.token(email, password).response();
+    return response.then(data => {
+      if (response.isError === false && data && data.token) {
+        TOKEN.set(data.token);
+      }
+      !callback || callback(response.isError, data);
+    });
+  };
 }
