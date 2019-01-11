81f08107e56cfead41af17069f6afc570899ae86
William Chakroun Jacobsen <williacj@stud.ntnu.no>
2019-01-11 11:45:23 +0100

    added fake methods

--- src/api/services/WarningService.js
+++ src/api/services/WarningService.js
@@ -4,13 +4,15 @@ import API from "../api";
 // and all the methods will return a promise
 export default class WarningService {
 
-    static getWarnings = (email, password, callback) => {
-        // Trim and uncapitalize email
-        email = email.trim().toLowerCase();
-
-        const response = AUTH.createUser(email, password).response();
-        return response.then(data => {
-            !callback || callback(response.isError, data);
-        });
+    static getWarnings = (callback) => {
+        return callback;
     };
+
+    static getWarning = (callback) =>{
+        return callback;
+    };
+
+    static createWarning = (callback) =>{
+        return callback;
+    }
 }
