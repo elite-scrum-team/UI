9a1a93a5e87e4ef069108703b88fd4c5964a1f26
rubensv <Rubvalen@gmail.com>
2019-01-11 13:43:56 +0100

    Added API logic? Maybe? idk?

--- src/containers/Details/index.js
+++ src/containers/Details/index.js
@@ -5,6 +5,9 @@ import {withStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Divider from '@material-ui/core/Divider';
 
+// Service import
+import WarningService from "../../api/services/WarningService";
+
 // Icons
 
 // Project components
@@ -45,6 +48,39 @@ const styles = {
 
 class Details extends Component {
 
+    state = {
+        title: null,
+        warnDate: null,
+        status: 1,
+        province: null,
+        statusMessage: null,
+        description: null,
+        location: {
+            lat: 0,
+            lng: 0,
+        },
+        images: null,
+        items: null,
+    };
+
+    componentDidMount() {
+        let temp = WarningService.getWarning(1);
+        console.log(temp)
+        if(temp!=null){
+            this.setState({
+                title: 'helo',
+                warnDate: temp.warnDate,
+                status: temp.status,
+                province: temp.province,
+                statusMessage: temp.statusMessage,
+                description: temp.description,
+                location: temp.location,
+                images: temp.images,
+                items: temp.items,
+            })
+        }
+    }
+
     render() {
         const {classes} = this.props;
         return (
@@ -52,13 +88,13 @@ class Details extends Component {
                 <div className={classes.root}>
                     <Paper elevation={1}>
                         <WarningDetails 
-                            title='Hull i vei'
-                            date='2019-01-09T21:39:59+01:00'
-                            status={3}
-                            province='Trondheim Kommune'
-                            statusMessage='Vi har begynt å fikse veien, og dette blir tatt hånd om så kjapt som mulig.'
-                            description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.'
-                            location={{lat: 63.429748, lng: 10.393916}}
+                            title={this.state.title}
+                            date={this.state.warnDate}
+                            status={this.state.status}
+                            province={this.state.province}
+                            statusMessage={this.state.statusMessage}
+                            description={this.state.description}
+                            location={this.state.location}
                             />
                         <Divider />
                         <ImageGrid
@@ -82,6 +118,7 @@ class Details extends Component {
                                 date='2019-01-09T21:39:59+01:00'
                                 statustekst='Work in Progress'
                                 statusMessage='Har sagt i fra til bedrift bla bla bla'
+                                items={this.state.items}
                             />
                         </div>
                     </div>
