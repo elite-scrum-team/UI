0e1ce925bd28b4f83fb269746e71e296edc6eba5
rubensv <Rubvalen@gmail.com>
2019-01-14 12:56:24 +0100

    Make dialog open and close woo but this time it's electric boogaloo

--- src/containers/Details/components/ActionModule.js
+++ src/containers/Details/components/ActionModule.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/styles';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/styles';
 import { useState } from 'react';
 import {Typography} from "@material-ui/core";
 
@@ -9,69 +9,70 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 
+
 // Icons
 
 // Project components
-import MessageDialog from '../../../components/miscellaneous/MessageDialog';
+import StatusDialog from './StatusDialog'
 
-const styles = makeStyles({
+const styles = {
     root: {
         
     }
-});
+};
 
-const ActionModule = (props) => {
-    // State
-    const [dialogOpen, setDialogOpen] = useState(false);
-    const [newStatus, setNewStatus] = useState(null);
+class ActionModule extends Component {
+    state = {
+        dialogOpen: false,
+        newStatus: -1,
+        statusMsg: '',
+    }
 
-    // Styling
-    const classes = styles();
-
-    const handleNewStatus = (value) => {
+    handleNewStatus = async (value) => {
+        await this.setState({newStatus: value});
         console.log(value);
-        setDialogOpen(false);
+        console.log("NewStatus: " + this.state.newStatus + ' ' + this.state.statusMsg);
+        this.setState({dialogOpen: (false)});
     };
 
-    return (
-        <div className={classes.root}>
-            <div>
-                <Typography variant={"h6"}>
-                    Actions:
-                </Typography>
-                <List component="nav" className={classes.root}>
-                    <ListItem button>
-                        <ListItemText primary="Varsle meg ved endringer" />
-                    </ListItem>
-                    <Divider />
-                    <ListItem button divider>
-                        <ListItemText primary="Registrer kontrakt" />
-                    </ListItem>
-                    <ListItem button onClick={() => setDialogOpen(true)}>
-                        <ListItemText primary="Ny status" />
-                    </ListItem>
-                    <Divider light />
-                </List>
-            </div>
-            <MessageDialog
-                title='Sett ny status:'
-                actions={[
-                    {label: 'Ikke godjkent', action: ()=>{handleNewStatus(0)}},
-                    {label: 'Godkjent', action: ()=>{handleNewStatus(1)}},
-                    {label: 'Arbeid påbegynt', action: ()=>{handleNewStatus(2)}},
-                    {label: 'Ferdig', action: ()=>{handleNewStatus(3)}},
-                    {label: 'Deaktivert', action: ()=>{handleNewStatus(4)}}
-                ]}
-                open={dialogOpen}
-            >
+    render() {
 
-            </MessageDialog>
-        </div>
-    )
+
+        // Styling
+        const {classes} = this.props;
+
+
+
+        return (
+            <div className={classes.root}>
+                <div>
+                    <Typography variant={"h6"}>
+                        Actions:
+                    </Typography>
+                    <List component="nav" className={classes.root}>
+                        <ListItem button>
+                            <ListItemText primary="Varsle meg ved endringer" />
+                        </ListItem>
+                        <Divider />
+                        <ListItem button divider>
+                            <ListItemText primary="Registrer kontrakt" />
+                        </ListItem>
+                        <ListItem button onClick={() => this.setState({dialogOpen: true})}>
+                            <ListItemText primary="Ny status" />
+                        </ListItem>
+                        <Divider light />
+                    </List>
+                </div>
+                <StatusDialog
+                    open={this.state.dialogOpen}
+                />
+            </div>
+        )
+    }
 }
 
 ActionModule.propTypes = {
-    
+
 }
 
-export default (ActionModule);
+export default withStyles(styles)(ActionModule);
--- src/containers/Details/components/StatusDialog.js
+++ src/containers/Details/components/StatusDialog.js
@@ -1,32 +1,89 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/styles';
+import {makeStyles, withStyles} from '@material-ui/styles';
 import { useState } from 'react';
 
 // Material UI components
+import TextField from '@material-ui/core/TextField';
 
 // Icons
 
 // Project components
+import MessageDialog from '../../../components/miscellaneous/MessageDialog';
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
-const styles = makeStyles({
+
+const styles = {
     root: {
-        
+
     }
-});
+};
 
-const StatusDialog = (props) => {
+class StatusDialog extends Component{
     // State
-    const [open, setData] = useState({});
+    state = {
+        dialogOpen: false,
+        newStatus: -1,
+        statusMsg: '',
+    }
 
-    // Styling
-    const classes = styles();
+    handleChange = (name) => (event) => {
+        this.setState({[name]: event.target.value});
+    };
 
-    return (
-        <div className={classes.root}>
-            
-        </div>
-    )
+    handleNewStatus = () =>{
+
+    };
+
+    render() {
+        // Styling
+        const {classes} = this.props;
+
+        return (
+            <div className={classes.root}>
+                <MessageDialog
+                    title='Sett ny status:'
+                    actions={[{label: 'Send', action: ()=> this.handleNewStatus()}]}
+                    open={this.props.open}
+                >
+                    <FormControl className={classes.formControl}>
+                        <InputLabel htmlFor="status-simple">Status:</InputLabel>
+                        <Select
+                            value={this.state.newStatus}
+                            onChange={this.handleChange('newStatus')}
+                            inputProps={{
+                                name: 'Status',
+                                id: 'status-simple',
+                            }}
+                        >
+                            <MenuItem value={0}>Ikke godkjent</MenuItem>
+                            <MenuItem value={1}>Godkjent</MenuItem>
+                            <MenuItem value={2}>Arbeid påbegynt</MenuItem>
+                            <MenuItem value={3}>Ferdig</MenuItem>
+                            <MenuItem value={4}>Avslått</MenuItem>
+
+                        </Select>
+                    </FormControl>
+                    <br/>
+                    <TextField
+                        id="filled-email-input"
+                        label="Statusmelding"
+                        className={classes.textField}
+                        type="StatusMessage"
+                        name="StatusMessage"
+                        autoComplete="statusMsg"
+                        margin="normal"
+                        onChange={this.handleChange('statusMsg')}
+                    />
+                </MessageDialog>
+            </div>
+        )
+    }
+
 }
 
-export default (StatusDialog);
+
+export default withStyles(styles)(StatusDialog);
