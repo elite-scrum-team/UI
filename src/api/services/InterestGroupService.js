4f1e7b0bb0313b0e05ad4fa55cbabd6029b47624
William Chakroun Jacobsen <williacj@stud.ntnu.no>
2019-01-22 16:09:41 +0100

    svein er faren til sveinung

---
+++ src/api/services/InterestGroupService.js
@@ -0,0 +1,23 @@
+import API from "../api";
+import AUTH from "../auth";
+
+// and all the methods will return a promise
+export default class InterestGroupService {
+
+    static subscribe = (warningId ,callback) => {
+        const response = API.subscribeToAWarning(warningId).response();
+        return response.then(async data => {
+            !callback || callback(response.isError, data);
+            return data;
+        });
+    };
+
+    static unsubscribe = (warningId ,callback) => {
+        const response = API.unSubscribeToAWarning(warningId).response();
+        return response.then(async data => {
+            !callback || callback(response.isError, data);
+            return data;
+        });
+    };
+
+}
